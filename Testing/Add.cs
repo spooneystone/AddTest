00594da3ac1cf757523b7b19b8797053ad319679
spooneystone <spooneystone@gmail.com>
2017-02-23 14:49:30 +0000

    Add project files.

---
+++ Testing/Add.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Add
+    {
+        int firstNumber, secondNumber;
+
+        public Add(int _numberToAdd1, int _numberToAdd2)
+        {
+            firstNumber = _numberToAdd1;
+            secondNumber = _numberToAdd2;
+        }
+
+        public int Result()
+        {
+            return firstNumber + secondNumber;
+        }
+
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add add = new Add(10,5);
+
+          Console.WriteLine(add.Result());
+            Console.ReadKey();
+        }
+    }
+}

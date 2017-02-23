939bfeda351507275707d1736ffb3b6b8f257c73
spooneystone <spooneystone@gmail.com>
2017-02-23 15:44:31 +0000

    Added a Divide

---
+++ Testing/Divide.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Divide
+    {
+        int firstNumber, secondNumber;
+
+        public Divide(int _numberToDiv1, int _numberToDiv2)
+        {
+            firstNumber = _numberToDiv1;
+            secondNumber = _numberToDiv2;
+        }
+
+        public int Result()
+        {
+            return firstNumber / secondNumber;
+        }
+
+    }
+}
